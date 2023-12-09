197e3e04fdccc99dd6071f42133d3855a3294b91
Deeptimayee Kisan <149052030+deeptimaye@users.noreply.github.com>
2023-12-09 19:41:13 +0530

    Add files via upload

---
+++ empolyee.java
@@ -0,0 +1,36 @@
+Package One;
+
+public class Docter {
+	private string docterName; 
+	private string docterLastName ;
+	private int docterID;
+	
+	public empolyee(String docterName,String docterLastName,int docterID) {
+		this.employeeName=empolyeeName;
+		this. empolyeeLastname=empolyeeLastName ;
+		this.empolyeeID=empolyeeID ;
+	}
+	public String  getempolyeeName() {
+	       return empolyeeName;
+	       
+	}
+	public void setemployeeName(String empolyeeName) {
+		    this.empolyeeName=empolyeeName ;
+	}
+	public String getempolyeeLastName() {
+		   return empolyeeLastName;
+	}
+	public  void setempolyeeLastName(String empolyeeLastName) {
+		   this.empolyeeLastName=empolyeeLastName;
+	}
+	public String getempolyeeID() {
+		   return empolyeeID;
+	}
+	public void setempolyeeID(String empolyeeID) {
+		   this.empolyeeID=empolyeeID;
+	}
+	public Static void main(String[] agrs) {
+		System.out.println("Successful");
+	}
+		   
+}
